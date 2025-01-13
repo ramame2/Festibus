cb43c6ab77481276c361d06f6f12146cd89bedad
Rama <s1206152@student.windesheim.nl>
2025-01-13 11:40:22 +0100

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>@yield('title')</title>
+    <link rel="icon" href="{{ asset('images/fff.jpg') }}" >
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     @if(!auth()->check() || auth()->user()->role !== 'admin')
         @php
             header('Location: ' . route('home'));
@@ -19,7 +25,7 @@
             <!-- Admin Bar -->
             <div class="admin-bar">
                 <ul>
-                    <li><a href="{{ route('admin.dashboard') }}">Welkom admin, {{ auth()->user()->name }}!</a></li>
+                    <li><a href="{{ route('admin.dashboard') }}">Welkom, {{ auth()->user()->name }}!</a></li>
                     <li><a href="{{ route('users.index') }}">Beheer Users</a></li>
                     <li><a href="{{ route('festivals.index') }}">Festivals</a></li>
                     <li><a href="{{ route('faq.index') }}"> FAQs</a></li>
